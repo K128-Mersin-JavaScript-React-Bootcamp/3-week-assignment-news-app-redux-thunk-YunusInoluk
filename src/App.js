9aa18561ac49bd3e652ebae2170024a391f211dc
Yunus Inoluk <yunusinoluk@gmail.com>
2021-10-14 20:14:55 +0300

    Added categories pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import CategoriesPage from "./pages/CategoriesPage";
+import NewsDetails from "./pages/NewsDetails";
+import NewsListPage from "./pages/NewsListPage";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/">
+          <CategoriesPage />
+        </Route>
+        <Route path="/news-list">
+          <NewsListPage />
+        </Route>
+        <Route path="/news-details">
+          <NewsDetails />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/NewsListPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NewsListPage() {
+  return <div>NewsListPage</div>;
+}
